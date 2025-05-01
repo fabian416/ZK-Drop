e8bf356f768798e152d38cd0c1175e4756db1485
fabiandiaz416 <diazfabian99@icloud.com>
2025-05-01 20:53:04 -0300

    change name module in gnss

--- zkapp/gnss.ts
+++ zkapp/gnss.ts
@@ -4,7 +4,7 @@ const { GNSSModule } = NativeModules;
 
 export const getGNSSCoordinates = async () => {
   try {
-    const coords = await GNSSModule.getCurrentLocation();
+    const coords = await GNSSModule.getGnssData(); 
     return coords;
   } catch (err) {
     console.error('GNSS error', err);
