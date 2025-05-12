2bcee73abd4731d6948ddb0465b6ec498606cd13
S1X3L4 <alexis.janko@gmail.com>
2025-05-12 04:38:40 -0300

    chore agrego deployment scipts

--- contracts/src/AirdropContracts.sol
+++ contracts/src/AirdropContracts.sol
@@ -1,24 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {BASE_ERC} from "./erc_tokens/BASE_ERC.sol";
+import {ZKL} from "./erc_tokens/ZKL.sol";
 import {HonkVerifier} from "./Verifier.sol";
 
 contract AirdropContracts {
-    BASE_ERC public base_erc;
+    ZKL public zkl;
     HonkVerifier public bonk_verifier;
 
     uint256 public airdrop_amount;
 
-    constructor(uint256 _airdrop_amount) {
-        base_erc = new BASE_ERC("BASE_ERC", "BASE", 1000000000000000000);
+    constructor(uint256 _airdrop_amount, uint256 circulating_supply) {
+        zkl = new ZKL();
         bonk_verifier = new HonkVerifier();
         airdrop_amount = _airdrop_amount;
+        zkl.mint(address(this), circulating_supply);
     }
 
     function airdrop(bytes memory proof, bytes32[] memory publicInputs) public {
         require(bonk_verifier.verify(proof, publicInputs), "Invalid proof");
-        base_erc.transfer(msg.sender, airdrop_amount);
+        zkl.transfer(msg.sender, airdrop_amount);
     }
 
 }
