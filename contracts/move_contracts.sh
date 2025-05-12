077d17579976c2025d902955817d6f6bea60076c
S1X3L4 <alexis.janko@gmail.com>
2025-05-12 05:02:58 -0300

    updating abis

--- contracts/move_contracts.sh
+++ contracts/move_contracts.sh
@@ -6,3 +6,4 @@ cp ./out/MockUSDC.sol/MockUSDC.json ../zk-front/src/lib/abis/MockUSDC.json
 cp ./out/ZKL.sol/ZKL.json ../zk-front/src/lib/abis/ZKL.json
 cp ./out/AirdropContracts.sol/AirdropContracts.json ../zk-front/src/lib/abis/AirdropContracts.json
 cp ./out/PreSale.sol/PreSale.json ../zk-front/src/lib/abis/PreSale.sol.json
+cp ./out/MockedAirdropContract.sol/MockedAirdropContract.json ../zk-front/src/lib/abis/MockedAirdropContract.sol.json
