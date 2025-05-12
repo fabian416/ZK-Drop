eb6ac818c7bfd99b673b388a66533779c4a523c4
S1X3L4 <alexis.janko@gmail.com>
2025-05-12 04:44:39 -0300

    script para mover abis

---
+++ contracts/move_contracts.sh
@@ -0,0 +1,8 @@
+
+forge build
+
+cp ./out/Verifier.sol/HonkVerifier.json ../zk-front/src/lib/abis/HonkVerifier.json
+cp ./out/MockUSDC.sol/MockUSDC.json ../zk-front/src/lib/abis/MockUSDC.json
+cp ./out/ZKL.sol/ZKL.json ../zk-front/src/lib/abis/ZKL.json
+cp ./out/AirdropContracts.sol/AirdropContracts.json ../zk-front/src/lib/abis/AirdropContracts.json
+cp ./out/PreSale.sol/PreSale.json ../zk-front/src/lib/abis/PreSale.sol.json
